a99e8cc1eca7c1e3f4b321e7f05ba91ecbea3cd7
ultraelephant <34905801+ultraelephant@users.noreply.github.com>
2018-07-26 18:46:09 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 echo "configuring modules for tft fb"
 echo "fbtft_device" >> /etc/modules-load.d/modules.conf
 echo "options fbtft_device custom name=adafruit18 gpios=reset:1,dc:0,led:3 width=128 height=128 busnum=1" > /etc/modprobe.d/fbtft.conf
+echo "blacklist bmp085" > /etc/modprobe.d/blacklist.conf
 
 echo "installing required packages"
 apt-get install python-pygame python-netifaces build-essential libi2c-dev i2c-tools python-dev libffi-dev -y
