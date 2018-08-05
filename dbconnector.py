272cb11828a47116f32fe5c88cba64efc938bb91
ultraelephant <34905801+ultraelephant@users.noreply.github.com>
2018-08-05 13:03:15 +1000

    Update dbconnector.py

--- dbconnector.py
+++ dbconnector.py
@@ -1,13 +1,10 @@
 import pymysql.cursors
-import sys
-sys.path.insert(0, '/etc/optimeteo')
-import config
 
-def tablecheck (tablename):
+def tablecheck (tablename,dbuser,dbpassword,dbname):
  connection = pymysql.connect(host='localhost',
-                              user=config.meteouser,
-                              password=config.meteouserpassword,
-                              db=config.meteobasename,
+                              user=dbuser,
+                              password=dbpassword,
+                              db=dbname,
                               charset='utf8mb4',
                               cursorclass=pymysql.cursors.DictCursor)
  cursor = connection.cursor()
@@ -15,4 +12,5 @@ def tablecheck (tablename):
  result = cursor.fetchall()
  if not result:
   cursor.execute("CREATE TABLE `%s` (`id` INT(11) NOT NULL AUTO_INCREMENT, `datetime` DATETIME(0) NOT NULL, `temp` DOUBLE(3,1) NOT NULL, `pres` DOUBLE(12,8) NOT NULL, `humi` DOUBLE(12,10) NOT NULL, PRIMARY KEY(`id`))" % (tablename))
+ connection.close()
  
