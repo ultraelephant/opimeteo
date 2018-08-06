9ae9a72cd6e3499ba0dd1a46c43f6237d681b0c6
ultraelephant <34905801+ultraelephant@users.noreply.github.com>
2018-08-06 23:26:54 +1000

    Update dbconnector.py

--- dbconnector.py
+++ dbconnector.py
@@ -11,6 +11,18 @@ def tablecheck (tablename,dbuser,dbpassword,dbname):
  cursor.execute("SHOW TABLES like '%s'" % (tablename))
  result = cursor.fetchall()
  if not result:
-  cursor.execute("CREATE TABLE `%s` (`id` INT(11) NOT NULL AUTO_INCREMENT, `datetime` DATETIME(0) NOT NULL, `temp` DOUBLE(3,1) NOT NULL, `pres` DOUBLE(12,8) NOT NULL, `humi` DOUBLE(12,10) NOT NULL, PRIMARY KEY(`id`))" % (tablename))
+  cursor.execute("CREATE TABLE `%s` (`id` INT(11) NOT NULL AUTO_INCREMENT, `datetime` DATETIME(0) NOT NULL, `temp` DOUBLE(4,2) NOT NULL, `pres` DOUBLE(12,8) NOT NULL, `humi` DOUBLE(12,10) NOT NULL, PRIMARY KEY(`id`))" % (tablename))
  connection.close()
  
+def commitdata (tablename,dbuser,dbpassword,dbname,temp,pres,humi):
+ tablecheck (tablename,dbuser,dbpassword,dbname)
+ connection = pymysql.connect(host='localhost',
+                              user=dbuser,
+                              password=dbpassword,
+                              db=dbname,
+                              charset='utf8mb4',
+                              cursorclass=pymysql.cursors.DictCursor)
+ cursor = connection.cursor()
+ cursor.execute ("INSERT INTO %s (datetime,temp,pres,humi) VALUES(CURRENT_TIMESTAMP(),'%s','%s','%s')" % (tablename,temp,pres,humi))
+ connection.commit()
+ connection.close()
