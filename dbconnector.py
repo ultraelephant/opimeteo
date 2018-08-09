2aad9da96021398f4656d40f4eee4737da1a7b35
ultraelephant <34905801+ultraelephant@users.noreply.github.com>
2018-08-09 23:57:08 +1000

    Update dbconnector.py

--- dbconnector.py
+++ dbconnector.py
@@ -11,18 +11,22 @@ def tablecheck (tablename,dbuser,dbpassword,dbname):
  cursor.execute("SHOW TABLES like '%s'" % (tablename))
  result = cursor.fetchall()
  if not result:
+  print ('Creating ' +tablename+ ' table')
   cursor.execute("CREATE TABLE `%s` (`id` INT(11) NOT NULL AUTO_INCREMENT, `datetime` DATETIME(0) NOT NULL, `temp` DOUBLE(4,2) NOT NULL, `pres` DOUBLE(12,8) NOT NULL, `humi` DOUBLE(12,10) NOT NULL, PRIMARY KEY(`id`))" % (tablename))
  connection.close()
  
 def commitdata (tablename,dbuser,dbpassword,dbname,temp,pres,humi):
- tablecheck (tablename,dbuser,dbpassword,dbname)
- connection = pymysql.connect(host='localhost',
-                              user=dbuser,
-                              password=dbpassword,
-                              db=dbname,
-                              charset='utf8mb4',
-                              cursorclass=pymysql.cursors.DictCursor)
- cursor = connection.cursor()
- cursor.execute ("INSERT INTO %s (datetime,temp,pres,humi) VALUES(CURRENT_TIMESTAMP(),'%s','%s','%s')" % (tablename,temp,pres,humi))
- connection.commit()
- connection.close()
+ try:
+  connection = pymysql.connect(host='localhost',
+                               user=dbuser,
+                               password=dbpassword,
+                               db=dbname,
+                               charset='utf8mb4',
+                               cursorclass=pymysql.cursors.DictCursor)
+  cursor = connection.cursor()
+  cursor.execute ("INSERT INTO %s (datetime,temp,pres,humi) VALUES(CURRENT_TIMESTAMP(),'%s','%s','%s')" % (tablename,temp,pres,humi))
+  connection.commit()
+  connection.close()
+ except pymysql.err.ProgrammingError:
+  tablecheck (tablename,dbuser,dbpassword,dbname)
+  commitdata (tablename,dbuser,dbpassword,dbname,temp,pres,humi)
