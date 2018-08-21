d8eb937453e8905d5c5f541508df485d722d6e78
ultraelephant <34905801+ultraelephant@users.noreply.github.com>
2018-08-21 23:59:44 +1000

    Update index.php

--- webapp/index.php
+++ webapp/index.php
@@ -5,11 +5,18 @@ include "dbconnector.php";
 
 $config = pyconfpars('/etc/optimeteo/config.py');
 
-echo "<select>";
+echo "<script src='js/jquery-3.3.1.min.js'></script>";
+echo "<script src='js/index.js'></script>";
+
+echo "<form method='post' action='getvalues.php'>";
+//echo "<select id='getvalue_dbname' name='getvalue_dbname' onchange='this.form.submit()'>";
+echo "<select id='getvalue_dbname'>";
 foreach (getdatatables($config['meteouser'],$config['meteouserpassword'],$config['meteobasename']) as $table) 
 {
- echo "<option value='" . $table . "'>" . $table . "</option>"; 
+ echo "<option value='" . $table . "'>" . $table . "</option>";
 }
 echo "</select>";
+echo "</form>";
+echo "<span id='values'></span>"
 
 ?>
